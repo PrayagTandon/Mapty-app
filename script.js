2635876fea815c523492015addf7e42d84c5e181
Prayag Tandon <tandonprayag99@gmail.com>
2024-02-09 20:20:52 +0530

    done with the leaflet library

--- script.js
+++ script.js
@@ -18,6 +18,20 @@ navigator.geolocation.getCurrentPosition(function (position) {
     console.log(longitude);
 
     console.log(`https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu`);
+
+    // Adding the leaflet library
+    //  To set the latitude and longitude for our position..
+    const coords = [latitude, longitude];
+    // 13 -> zoom level
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker(coords).addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
 }, function () {
     alert(`Couldn't retreive your location ☹️☹️`);
 }, { enableHighAccuracy: true, timeout: 10000 })
