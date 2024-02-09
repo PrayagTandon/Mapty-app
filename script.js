5040ed424cf1b6c5247160fec0ef93db3623b10b
Prayag Tandon <tandonprayag99@gmail.com>
2024-02-09 19:32:24 +0530

    ...

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 // The geolocation API
+navigator.geolocation.getCurrentPosition(function (position) {
+    const { latitude, longitude } = position.coords;
+    console.log(latitude);
+    console.log(longitude);
+
+    console.log(`https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu`);
+}, function () {
+    alert(`Couldn't retreive your location ☹️☹️`);
+}, { enableHighAccuracy: true, timeout: 10000 })
