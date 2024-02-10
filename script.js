607e2dc63ff79b2511509e5720c92bbb58f293d2
Prayag Tandon <tandonprayag99@gmail.com>
2024-02-10 12:38:54 +0530

    ...

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+// variable definition
+let map, mapEvent;
+
 /* The Geolocation API and the Leaflet Library */
 
 navigator.geolocation.getCurrentPosition(function (position) {
@@ -20,31 +23,47 @@ navigator.geolocation.getCurrentPosition(function (position) {
     //  To set the latitude and longitude for our position..
     const coords = [latitude, longitude];
     // 13 -> zoom level
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
     // Adding the event listener to map object using on() method
-    map.on('click', function (mapEvent) {
+    map.on('click', function (mapE) {
+        mapEvent = mapE;
         form.classList.remove('hidden');
         inputDistance.focus();
-
-        // const { lat, lng } = mapEvent.latlng;
-
-        // L.marker([lat, lng]).addTo(map)
-        //     .bindPopup(L.popup({
-        //         maxWidth: 250,
-        //         minWidth: 100,
-        //         autoClose: false,
-        //         closeOnClick: false,
-        //         className: 'running-popup',
-        //     }))
-        //     .setPopupContent('Workout!')
-        //     .openPopup();
     })
 
 }, function () {
     alert(`Couldn't retreive your location ☹️☹️`);
-}, { enableHighAccuracy: true, timeout: 10000 })
+}, { enableHighAccuracy: true, timeout: 10000 });
+
+// EVENT LISTENER FOR FORM
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // Clearing Input fields
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+    // Display marker
+    const { lat, lng } = mapEvent.latlng;
+
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+        }))
+        .setPopupContent('Workout!')
+        .openPopup();
+});
+
+// Adding the listener to the select -> change event
+inputType.addEventListener('change', function () {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+})
