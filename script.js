e2d0bb40c76f81685592da6a2fbef696f1724611
Prayag Tandon <tandonprayag99@gmail.com>
2024-02-11 14:14:40 +0530

    ...

--- script.js
+++ script.js
@@ -61,6 +61,7 @@ class App {
     // Private instance fields
     #map;
     #mapEvent;
+    #workouts = [];
 
     constructor() {
         this.#getPosition();
@@ -107,26 +108,55 @@ class App {
     }
 
     #newWorkout(e) {
+        // Creating helper functions to clean our main code...
+        const validInput = function (...inputs) {
+            return inputs.every(input => Number.isFinite(input));
+        };
+
+        const allPositive = function (...inputs) {
+            return inputs.every(input => input >= 0);
+        }
+
         e.preventDefault();
+
 
         // Get data from form
         const type = inputType.value;
         const distance = +inputDistance.value;
         const duration = +inputDuration.value;
+        const { lat, lng } = this.#mapEvent.latlng;
+        let workout;
+
 
         // If workout running , create running object
         if (type === 'running') {
             const cadence = +inputCadence.value;
-            // Check if data is valid
-            if (!Number.isFinite(distance) || !Number.isFinite(duration) || !Number.isFinite(cadence)) return alert(`The provided input is incorrect⛔⛔`);
-        }
+            // Check if data is valid -> Guard Clause
+            if (!validInput(distance, duration, cadence) || !allPositive(distance, duration, cadence)) {
+                return alert(`The provided input is incorrect⛔⛔`);
+            }
+
+            workout = new Running([lat, lng], distance, duration, cadence);
+        };
+
 
         // If workout cycling , create cycling object
+        if (type === 'cycling') {
+            const elevation = +inputElevation.value;
+            // Check if data is valid -> Guard Clause
+            if (!validInput(distance, duration, elevation) || !allPositive(distance, duration)) {
+                return alert(`The provided input is incorrect⛔⛔`);
+            }
+
+            workout = new Cycling([lat, lng], distance, duration, elevation);
+        };
+
 
         // Add new object to the workout array
+        this.#workouts.push(workout);
+
 
         // Render workout on map as marker
-        const { lat, lng } = this.#mapEvent.latlng;
 
         L.marker([lat, lng]).addTo(this.#map)
             .bindPopup(L.popup({
